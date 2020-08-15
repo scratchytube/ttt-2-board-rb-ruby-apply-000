5aec761511bf9256107e073c969617e9dea5ca0d
Brian Sahota <bpsahota@gmail.com>
2020-08-15 00:22:40 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,1 @@
-# Define the variable board below.
-board = " " " " " "
-board = " " " " " "
-board = " " " " " "
-
-row[0] = 1
-row[1] = 2
-row[2] = 3
-row[3] = 4
-row[4] = 5
-row[5] = 6
-row[6] = 7
-row[7] = 8
-row[8] = 9
+board = " " " " " " " " " " " " " " " " " "
